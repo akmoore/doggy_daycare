c1be103b223f5d44c0bb9d28481c10eb0c242a68
Alfred Moore <amoore@Alfreds-MacBook-Pro.local>
2017-11-15 09:35:36 -0600

    Add owners and setup relationship to dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -51,7 +51,7 @@ class DogsController < ApplicationController
     private
 
     def dog_params
-        params.require(:dog).permit(:name, :age, :breed, :owner)
+        params.require(:dog).permit(:name, :age, :breed, :owner_id)
     end 
 
     def set_dog
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :owners
   # resources :dogs, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :dogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
